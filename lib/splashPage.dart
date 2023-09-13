6bf58f37646f0d92de0235159f8b56b9e114f0b8
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-09-13 10:54:01 +0530

    commit

--- lib/splashPage.dart
+++ lib/splashPage.dart
@@ -13,23 +13,36 @@ class SplashPage extends StatelessWidget {
   //to create our widget tree we use build function
   Widget build(BuildContext context) {
     //BuildContext - used to locate widgets on the widget tree and monitor their actions
-    return  Scaffold(
-      backgroundColor: Colors.yellow,
+    return const Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
-              Icons.favorite,
-              size: 80,
-              color: Colors.red,
+            ///adding built in icons
+            // Icon(
+            //   Icons.favorite,
+            //   size: 80,
+            //   color: Colors.red,
+            // ),
+            /// adding external icons (flutter accessed it as image)
+            /// Asset image as image provider
+            // Image(
+            //   image: AssetImage("assets/icons/tree.png"),
+            //   width: 100,
+            //   height: 100,
+            // ),
+            /// network image as image provider
+            Image(
+                image: NetworkImage("https://cdn1.iconfinder.com/data/icons/business-and-finance-flat/512/solving_puzzle_issue_thinking_solution-512.png"),
+                width: 100, height: 100,
             ),
             Text(
               "My Application",
               style: TextStyle(
                   fontSize: 20,
-                  color: Colors.green,
-              fontWeight: FontWeight.bold),
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold),
             )
           ],
         ),
