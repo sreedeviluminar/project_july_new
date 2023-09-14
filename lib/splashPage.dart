20ba444d64101502969428764a54a5684246cec4
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-09-14 11:14:12 +0530

    commit

--- lib/splashPage.dart
+++ lib/splashPage.dart
@@ -14,43 +14,59 @@ class SplashPage extends StatelessWidget {
   //to create our widget tree we use build function
   Widget build(BuildContext context) {
     //BuildContext - used to locate widgets on the widget tree and monitor their actions
-    return  Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ///adding built in icons
-            // Icon(
-            //   Icons.favorite,
-            //   size: 80,
-            //   color: Colors.red,
-            // ),
-            /// adding external icons (flutter accessed it as image)
-            /// Asset image as image provider
-            // Image(
-            //   image: AssetImage("assets/icons/tree.png"),
-            //   width: 100,
-            //   height: 100,
-            // ),
-            /// network image as image provider
-            Image(
-                image: NetworkImage("https://cdn1.iconfinder.com/data/icons/business-and-finance-flat/512/solving_puzzle_issue_thinking_solution-512.png"),
-                width: 100, height: 100,
-            ),
-            Text(
-              "My Application",
-              style: GoogleFonts.dancingScript(
-                fontSize: 30,
-                color: Colors.lightBlueAccent,
-                fontWeight: FontWeight.bold
+    return Scaffold(
+    //  backgroundColor: Colors.black,
+      body: Container(
+        decoration: const BoxDecoration(
+         // color: Colors.black38
+         //  image: DecorationImage(
+         //      fit: BoxFit.cover,
+         //      image: NetworkImage("https://plus.unsplash.com/premium_photo-1672582776474-fc85ebaa1b73?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"))
+        gradient: LinearGradient(
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+            colors: [
+          Colors.green,
+          Colors.white70,
+          Colors.black12
+        ])
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ///adding built in icons
+              // Icon(
+              //   Icons.favorite,
+              //   size: 80,
+              //   color: Colors.red,
+              // ),
+              /// adding external icons (flutter accessed it as image)
+              /// Asset image as image provider
+              // Image(
+              //   image: AssetImage("assets/icons/tree.png"),
+              //   width: 100,
+              //   height: 100,
+              // ),
+              /// network image as image provider
+              const Image(
+                  image: NetworkImage("https://cdn1.iconfinder.com/data/icons/business-and-finance-flat/512/solving_puzzle_issue_thinking_solution-512.png"),
+                  width: 100, height: 100,
               ),
-              // style: TextStyle(
-              //     fontSize: 20,
-              //     color: Colors.blue,
-              //     fontWeight: FontWeight.bold),
-            )
-          ],
+              Text(
+                "My Application",
+                style: GoogleFonts.dancingScript(
+                  fontSize: 30,
+                  color: Colors.lightBlueAccent,
+                  fontWeight: FontWeight.bold
+                ),
+                // style: TextStyle(
+                //     fontSize: 20,
+                //     color: Colors.blue,
+                //     fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
         ),
       ),
     );
