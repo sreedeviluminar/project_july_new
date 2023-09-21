a1b480ee9dee4c1bc28df2f9298e9faf5cb22a72
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-09-21 11:12:48 +0530

    commit

---
+++ lib/listview example/listview_constructor.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.teal),
+    home: ListView1(),
+  ));
+}
+
+class ListView1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ListView 1"),
+      ),
+      body: ListView(
+        children: const [
+          Card(
+              child: ListTile(
+            leading: Icon(Icons.people_alt_rounded),
+            title: Text('Product 1'),
+            subtitle: Text("This is a sample subtitle"),
+            trailing: Text('10.11'),
+          )),
+          Card(
+              child: ListTile(
+            leading: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/person.jpeg")),
+            title: Text('Product 1'),
+            subtitle: Text("This is a sample subtitle"),
+            trailing: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('10.11'),
+                CircleAvatar(
+                  minRadius: 6,
+                  maxRadius: 12,
+                  backgroundColor: Colors.teal,
+                  child: Text('2'),
+                )
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
