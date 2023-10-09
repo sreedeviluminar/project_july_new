130ebda095edf1551a2a9fcf919a17ae52dcdc62
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-10-09 11:25:38 +0530

    commit

---
+++ lib/carosal slider.dart
@@ -0,0 +1,116 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:project_july_new/fi%20card%20using_stack.dart';
+
+void main() {
+  runApp(MaterialApp(home: Car_Slider()));
+}
+
+class Car_Slider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Slider"),
+      ),
+      body: CarouselSlider(
+          items: [
+            GestureDetector(
+              onLongPress: () {
+                showSheet(context);
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://images.unsplash.com/photo-1696257203553-20ada15fce65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=900&q=60"))),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => FiCard()));
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60"))),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1591779051696-1c3fa1469a79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60"))),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1696257203553-20ada15fce65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=900&q=60"))),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60"))),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1591779051696-1c3fa1469a79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60"))),
+            )
+          ],
+          options: CarouselOptions(
+              autoPlay: true,
+              viewportFraction: .6,
+              height: 200,
+              enlargeCenterPage: true,
+              enableInfiniteScroll: true,
+              autoPlayCurve: Curves.fastLinearToSlowEaseIn,
+              autoPlayAnimationDuration: const Duration(seconds: 2))),
+    );
+  }
+
+  void showSheet(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return const Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Share Via",
+                style: TextStyle(fontSize: 20),
+              ),
+              Divider(
+                color: Colors.black26,
+              ),
+              ListTile(
+                leading: FaIcon(
+                  FontAwesomeIcons.whatsapp,
+                  color: Colors.green,
+                ),
+                title: Text('WhatsApp'),
+              ),
+              ListTile(
+                  leading: FaIcon(
+                    FontAwesomeIcons.instagram,
+                    color: Colors.pink,
+                  ),
+                  title: Text('Instagram'))
+            ],
+          );
+        });
+  }
+}
