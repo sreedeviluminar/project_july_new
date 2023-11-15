fd39176725bd8babea4de411d84ce240c1402a5c
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-11-15 11:00:08 +0530

    commit

--- lib/storages/firebase_ex/storage_ex.dart
+++ lib/storages/firebase_ex/storage_ex.dart
@@ -62,14 +62,29 @@ class _Fire_StorageState extends State<Fire_Storage> {
             ),
             Expanded(
                 child: FutureBuilder(
-
-                    ///if firebase connection is success then executes this function
+                  ///if firebase connection is success then executes this function
                     future: loadData(),
                     builder: (context,
                         AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
                       if (snapshot.connectionState == ConnectionState.done) {
                         return ListView.builder(
-                            itemBuilder: (context, index) {});
+                            itemCount: snapshot.data?.length ?? 0,
+                            itemBuilder: (context, index) {
+                              /// fetching each map from the list
+                              final Map<String, dynamic> image =
+                                  snapshot.data![index];
+                              return Card(
+                                child: ListTile(
+                                  // leading:Image.network(snapshot.data![index]['imageurl']) ,  OR
+                                  leading: Image.network(image['imageurl']),
+                                  title: Text(image['uploaded_by']),
+                                  subtitle: Text(image['description']),
+                                  trailing: IconButton(
+                                      onPressed: () => deleteImage(image['path']),
+                                      icon: const Icon(Icons.delete)),
+                                ),
+                              );
+                            });
                       } else {
                         return const Center(
                           child: CircularProgressIndicator(),
@@ -82,7 +97,28 @@ class _Fire_StorageState extends State<Fire_Storage> {
     );
   }
 
-  loadData() {}
+  Future<List<Map<String, dynamic>>> loadData() async {
+    List<Map<String, dynamic>> images = [];
+    final ListResult result = await storage
+        .ref()
+        .list(); // group of values(firebasestorage instance) from firebase storage
+    //all the data from firebase stored as a reference
+    final List<Reference> allfiles = result.items;
+
+    await Future.forEach(allfiles, (singlefile) async {
+      //to get image path (path as network image path)
+      final String fileUrl = await singlefile.getDownloadURL();
+      final FullMetadata metadata = await singlefile.getMetadata();
+
+      images.add({
+        'imageurl': fileUrl,
+        'path': singlefile.fullPath,
+        'uploaded_by': metadata.customMetadata?['uploaded_by'] ?? "No Data",
+        'description': metadata.customMetadata?['description'] ?? "No Data"
+      });
+    });
+    return images;
+  }
 
   Future<void> upload(String imageinput) async {
     final picker = ImagePicker();
@@ -111,4 +147,8 @@ class _Fire_StorageState extends State<Fire_Storage> {
       print(e);
     }
   }
+ Future<void> deleteImage(String imagepath) async{
+    await storage.ref(imagepath).delete();
+    setState(() {});
+ }
 }
